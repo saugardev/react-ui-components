0fb6fed019cb4e7f903efbaeb6dfeb52523ccc9c
Saúl García Céspedes <saul@dixwe.com>
2023-05-15 13:52:26 +0200

    feat: setup test and husky

--- src/jest.config.cjs
+++ src/jest.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   transform: {
-  '^.+\.tsx?$': 'ts-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
