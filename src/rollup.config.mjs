dbbcc241904cd4e82d9fde542de0ae1dd12d405b
Saúl García Céspedes <saul@dixwe.com>
2023-05-15 17:36:47 +0200

    feat: build library esmodule compatible

--- src/rollup.config.mjs
+++ src/rollup.config.mjs
@@ -7,11 +7,6 @@ export default {
   input: 'src/index.tsx',
   output: [
     {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
       file: pkg.module,
       format: 'es',
       sourcemap: true,
